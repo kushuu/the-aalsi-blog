5439875646c830c06e3430253ff7e3bfaac5ae95
kushuu <sonikushu007@gmail.com>
2021-06-27 19:02:28 +0530

    added page scroll indicator

--- static/main.js
+++ static/main.js
@@ -98,42 +98,11 @@ window.addEventListener("load", hidePreloader);
 
 
 // for scroller 
-function scrollProgressBar() {
-    var getMax = function () {
-      return $(document).height() - $(window).height();
-    };
-  
-    var getValue = function () {
-      return $(window).scrollTop();
-    };
-  
-    var progressBar = $(".progress-bar"),
-      max = getMax(),
-      value,
-      width;
-  
-    var getWidth = function () {
-      // Calculate width in percentage
-      value = getValue();
-      width = (value / max) * 100;
-      width = width + "%";
-      return width;
-    };
-  
-    var setWidth = function () {
-      progressBar.css({ width: getWidth() });
-    };
-  
-    $(document).on("scroll", setWidth);
-    $(window).on("resize", function () {
-      // Need to reset max
-      max = getMax();
-      setWidth();
-    });
-}
+window.onscroll = function() {myFunction()};
 
-$(document).ready(function () {
-    SF_scripts();
-  
-    scrollProgressBar();
-});
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
