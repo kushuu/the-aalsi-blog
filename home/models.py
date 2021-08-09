0e5c5a557ac37839db7b8ccd662a4c792471d4e8
kushuu <sonikushu007@gmail.com>
2021-08-09 10:35:23 +0530

    made datetime saved, relative

--- home/models.py
+++ home/models.py
@@ -11,7 +11,7 @@ class Contact(models.Model):
     email = models.EmailField(max_length=50)
     phone = models.CharField(max_length = 13)
     message = models.CharField(max_length=250)
-    date = models.DateTimeField()
+    date = models.DateTimeField(default=django.utils.timezone.now)
 
     def __str__(self):
         return self.email
