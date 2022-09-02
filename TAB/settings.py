92dc60d8514adb2e5462dd755e6254e70dc2a8c4
kushagra <sonikushu007@gmil.com>
2022-09-02 15:14:27 +0000

    updates from PA

--- TAB/settings.py
+++ TAB/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = '0ars@j2bn6p(p1(7+ojph^wk&em$yd+cs=*abenojfg11+suve'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
